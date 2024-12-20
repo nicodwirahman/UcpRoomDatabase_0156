7044d358ea383c8e56f8692670472fcdfbff39d8
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-20 23:05:06 +0700

    validasi tidak boleh kosong

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DosenViewModel.kt
@@ -1,8 +1,35 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.repository.RepositoryDosen
 
 
+class DosenViewModel(private val repositoryDosen: RepositoryDosen): ViewModel(){
+
+    var uiState by mutableStateOf(DosenUiState())
+
+    fun updateState(dosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            DosenEvent= dosenEvent
+        )
+    }
+
+    private fun validateFields(): Boolean{
+        val event = uiState.DosenEvent
+        val errorState = FormErrorState(
+            nidn = if (event.nidn.isNotEmpty()) null else "nidn tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "nama tidak boleh kosong",
+            JenisKelamin  = if (event.JenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+        )
+        uiState = uiState.copy(isEntryValid =  errorState)
+        return errorState.isValid()
+    }
+
+}
 data class DosenUiState(
     val DosenEvent: DosenEvent = DosenEvent(),
     val isEntryValid: FormErrorState = FormErrorState(),
