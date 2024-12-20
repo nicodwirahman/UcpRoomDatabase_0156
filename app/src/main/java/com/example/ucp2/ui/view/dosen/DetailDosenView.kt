87692d9390f5f45b91a174703ccd2d9dbf83be8a
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-20 21:31:02 +0700

    membuat fungsi tombol

--- app/src/main/java/com/example/ucp2/ui/view/dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/DetailDosenView.kt
@@ -6,20 +6,41 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import com.example.ucp2.data.entity.Dosen
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.Costumwidget.CustomTopAppBar
+
+@Composable
+fun DetailDosenView(
+    modifier: Modifier = Modifier,
+    viewModel: ViewModel = viewModel(),
+    onBack: () -> Unit = {},
+){
+    Scaffold (
+        topBar = {
+            CustomTopAppBar(
+                judul = "Detail Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+    )
+        },
 
 
+
+}
 @Composable
 fun ItemDetailDosen(
     modifier: Modifier = Modifier,
