f7ca3f1d28f53fe330cad392bd1506182856f078
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-20 18:30:14 +0700

    Menambahkan komponen detail dosen

--- app/src/main/java/com/example/ucp2/ui/view/dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/DetailDosenView.kt
@@ -1,14 +1,56 @@
 package com.example.ucp2.ui.view.dosen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.Dosen
+
+
+@Composable
+fun ItemDetailDosen(
+    modifier: Modifier = Modifier,
+    dosen: Dosen
+){
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+        ){
+            componentDetailDosen(judul = "ndin", isinya = dosen.nidn)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            componentDetailDosen(judul = "Nama", isinya = dosen.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            componentDetailDosen(judul = "jenis Kelamin", isinya = dosen.JenisKelamin)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+
+    }
+
+
+
+
+}
 
 @Composable
 fun componentDetailDosen(
