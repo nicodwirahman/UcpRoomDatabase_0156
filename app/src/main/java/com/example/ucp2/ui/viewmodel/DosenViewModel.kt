0122c72ba4a51b7b2c793bd0e633586a183b305e
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-20 22:45:57 +0700

    membuat data class DOsen Ui state

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DosenViewModel.kt
@@ -3,7 +3,11 @@ package com.example.ucp2.ui.viewmodel
 import com.example.ucp2.data.entity.Dosen
 
 
-
+data class DosenUiState(
+    val DosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
 
 data class FormErrorState(
     val nidn: String? =null,
