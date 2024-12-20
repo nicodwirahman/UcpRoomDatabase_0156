8544d52d44513be786b1e0fa08e3cfb3165cede3
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-20 23:34:09 +0700

    Konversi `Dosen` ke event UI.

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailDosenViewModel.kt
@@ -1,18 +1,19 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.repository.RepositoryDosen
+import com.example.ucp2.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.StateFlow
 
 
-
-data class DetailUiEvent(
-    val detailUiEvent : DosenEvent
-)
 
 fun Dosen.toDetailUiEvent(): DosenEvent{
-   return DosenEvent(
-       nidn = nidn,
-       nama = nama,
-       JenisKelamin = JenisKelamin,
-
-   )
+    return DosenEvent(
+        nidn = nidn,
+        nama = nama,
+        JenisKelamin = JenisKelamin
+    )
 }
+
