f98b246493f3b7862ed6b2644fc4b7eb74234e82
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-21 21:08:41 +0700

    Membuat Detail Dosen

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailDosenViewModel.kt
@@ -2,10 +2,50 @@ package com.example.ucp2.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Dosen
 import com.example.ucp2.repository.RepositoryDosen
 import com.example.ucp2.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class DetailDosenViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDosen: RepositoryDosen,
+): ViewModel() {
+
+    private val _ndin: String = checkNotNull(savedStateHandle[DestinasiDetail.ndin])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryDosen.getDosen(_ndin)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "terjadi kesalahan" // Fixed formatting issues
+                )
+            )
+        }
+        .stateIn(viewModelScope, SharingStarted.Lazily, DetailUiState(isLoading = true)) // Added stateIn for flow handling
+}
 
 
 data class DetailUiState(
@@ -21,6 +61,7 @@ data class DetailUiState(
         get() = detailUiEvent != DosenEvent()
 
 }
+
 fun Dosen.toDetailUiEvent(): DosenEvent{
     return DosenEvent(
         nidn = nidn,
@@ -28,4 +69,5 @@ fun Dosen.toDetailUiEvent(): DosenEvent{
         JenisKelamin = JenisKelamin
     )
 }
+
 
