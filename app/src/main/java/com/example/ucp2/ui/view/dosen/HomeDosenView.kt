06ef95ade826a60fd7fd6fc340d484625d066e69
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-21 22:23:53 +0700

    membuat tampilan nama dosen

--- app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDosenView.kt
@@ -1,2 +1,64 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.Dosen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardDosen(
+    dosen: Dosen,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit ={}
+) {
+
+    Card(
+        onClick =  onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+
+    ) {
+        Column (
+            modifier = modifier.padding(8.dp)
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    imageVector = Icons.Filled.Person, contentDescription = "" )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dosen.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+
+            }
+
+            Row (
+
+            ){
+
+            }
+
+        }
+    }
+
+}
