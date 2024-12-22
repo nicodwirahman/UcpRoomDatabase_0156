8930fd514ff3eb9296526e4e237483b4a75a2506
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-22 23:30:04 +0700

    membuat dataclass detailuiState dan fungsi Matakuliah

--- app/src/main/java/com/example/ucp2/ui/ViewModelMk/DetailMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/ViewModelMk/DetailMkViewModel.kt
@@ -1,84 +1,28 @@
 package com.example.ucp2.ui.ViewModelMk
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
 import com.example.ucp2.data.entity.MataKuliah
-import com.example.ucp2.repository.RepositoryMK
 
+data class DetailUiState(
+    val detailUiEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError : Boolean =false,
+    val errorMessage : String = ""
 
-class MataKuliahViewModel(private val repositoryMK: RepositoryMK): ViewModel() {
-    var uiState by mutableStateOf(MkUIState())
-
-    fun updatestate(mataKuliahEvent: MataKuliahEvent) {
-        uiState = uiState.copy(
-            mataKuliahEvent = mataKuliahEvent,
-        )
-    }
-
-    private fun validateFields(): Boolean {
-        val event = uiState.mataKuliahEvent
-        val errorState = FormErrorState(
-            Kode = if (event.Kode.isNotEmpty()) null else "Kode tidak boleh kosong",
-            Nama = if (event.Nama.isNotEmpty()) null else "NAMA tidak boleh kosong",
-            SKS = if (event.SKS.isNotEmpty()) null else "SKS tidak boleh kosong",
-            Semester = if (event.Semester.isNotEmpty()) null else "Semester tidak boleh kosong",
-            Jenis = if (event.Jenis.isNotEmpty()) null else "Jenis tidak boleh kosong",
-            DosenPengampu = if (event.DosenPengampu.isNotEmpty()) null else "Dosen Pengampu tidak boleh kosong"
-
-        )
-        uiState = uiState.copy(isEntryValid = errorState)
-        return errorState.isValid()
-    }
-
-    fun saveData(){
-        val currentEvent =uiState.mataKuliahEvent
-        if (validateFields()){
-            try {
-                repositoryMK.insertMK(currentEvent.toMataKuliahEntity())
-                uiState = uiState
-            }
-            }
-
-
-    }    }
-
-
-data class MkUIState(
-    val mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
-    val isEntryValid: FormErrorState = FormErrorState(),
-    val snackbarMessage: String? = null,
-)
-data class FormErrorState(
-    val Kode: String? = null,
-    val Nama: String? = null,
-    val SKS: String? = null,
-    val Semester: String? =null,
-    val Jenis: String? = null,
-    val DosenPengampu: String? = null
 ){
-    fun isValid(): Boolean{
-        return Kode == null && Nama == null && SKS == null &&
-                Semester == null && Jenis == null && DosenPengampu == null
-    }
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MataKuliahEvent()
 
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MataKuliahEvent()
 }
-data class MataKuliahEvent(
-    val Kode: String ="",
-    val Nama: String="",
-    val SKS: String="",
-    val Semester: String="",
-    val Jenis: String="",
-    val DosenPengampu: String=""
 
-)
-fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
-    kode = Kode,
-    Nama = Nama,
-    Sks = SKS,
-    Semester = Semester,
-    Jenis = Jenis,
-    DosenPengampu =DosenPengampu
-
-)
+fun MataKuliah.toDetailUiEvent(): MataKuliahEvent{
+    return MataKuliahEvent(
+        Kode = kode,
+        Nama = Nama,
+        SKS = Sks,
+        Semester = Semester,
+        Jenis = Jenis,
+        DosenPengampu = DosenPengampu
+    )
+}
