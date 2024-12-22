57217f618812a214e8150277006f54de81d4d58c
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-22 10:57:16 +0700

    yang sebelumnya salah push sekarang membuat tampilan nama dan nidn dosen

--- app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDosenview.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDosenview.kt
@@ -1,3 +1,57 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import com.example.ucp2.ui.viewmodel.DosenEvent
+import com.example.ucp2.ui.viewmodel.FormErrorState
+import org.w3c.dom.Text
+
+
+@Composable
+fun FormDosen(
+    dosenEvent: DosenEvent = DosenEvent(),
+    onValueChange: (DosenEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+) {
+    val jenisKelamin = listOf("laki-laki", "perempuan")
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nama,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan nama") }
+        )
+        if (errorState.nama != null) {
+            Text(
+                text = errorState.nama,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = dosenEvent.nidn, onValueChange = {
+                    onValueChange(dosenEvent.copy(nidn = it))
+                },
+                label = { Text("nidn") },
+                isError = errorState.nidn != null,
+                placeholder = { Text("Masukkan nidn") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Text(text = errorState.nidn ?: "", color = Color.Red)
+        }
+    }
+}
