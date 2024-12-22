2dd965637771d10d942cf1165e19ffac0a03e447
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-22 13:38:52 +0700

    membuat peyendia model deosen

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.ucp2.ui.viewmodel
+
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.UCP2App
+import com.example.ucp2.ui.view.dosen.DetailDosenView
+import com.google.android.ads.mediationtestsuite.viewmodels.ViewModelFactory
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                UCP2App().containerApp.RepositoryDosen
+            )
+        }
+        initializer {
+            HomeDosenViewModel(
+                UCP2App().containerApp.RepositoryDosen
+            )
+        }
+        initializer {
+            DetailDosenViewModel(
+                createSavedStateHandle(),
+                UCP2App().containerApp.RepositoryDosen
+            )
+        }
+    }
+
+    }
+
