ad81d0319bf199a4753a0eb7b4fd65edd879c842
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-22 11:19:31 +0700

    Menampilkan formulir tambah dosen dengan validasi.

--- app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDosenview.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDosenview.kt
@@ -1,27 +1,95 @@
 package com.example.ucp2.ui.view.dosen
 
+import android.text.Spannable.Factory
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.Costumwidget.CustomTopAppBar
 import com.example.ucp2.ui.viewmodel.DosenEvent
 import com.example.ucp2.ui.viewmodel.DosenUiState
+import com.example.ucp2.ui.viewmodel.DosenViewModel
 import com.example.ucp2.ui.viewmodel.FormErrorState
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 import org.w3c.dom.Text
 
+
+@Composable
+fun InsertDosenView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DosenViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val  uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message) // Tampilkan snackbar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+Scaffold (
+modifier = modifier,
+snackbarHost = { SnackbarHost(hostState = snackbarHostState)} // Tempatkan snackbar di scaffold
+){ padding ->
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+            .padding(16.dp)
+    ) {
+        CustomTopAppBar(
+            onBack = onBack,
+            showBackButton = true,
+            judul = "Tambah Dosen"
+        )
+        // isi Body
+        InsertBodyDosen(
+            uiState = uiState,
+            onValueChange = { updateEvent ->
+                viewModel.updateState(updateEvent) // Update state di ViewModel
+            },
+            onClick = {
+                coroutineScope.launch {
+                    viewModel.savedata() //simpan data
+                }
+                onNavigate()
+            }
+        )
+
+    }
+}
+}
 
 
 
