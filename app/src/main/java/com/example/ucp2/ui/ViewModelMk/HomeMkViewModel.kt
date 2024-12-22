ec2736b78d9f26edc895957d241601e2699069e6
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-22 23:58:39 +0700

    membuat class HomeMkViewModel

--- app/src/main/java/com/example/ucp2/ui/ViewModelMk/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/ViewModelMk/HomeMkViewModel.kt
@@ -1,7 +1,54 @@
 package com.example.ucp2.ui.ViewModelMk
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositoryMK
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
+class HomeMkViewModel (
+    private val repositoryMK: RepositoryMK
+): ViewModel(){
+    val homeUiState: StateFlow<HomeUiState> = repositoryMK.getAllMataKuliah()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMk = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState(
     val listMk: List<MataKuliah> = listOf(),
