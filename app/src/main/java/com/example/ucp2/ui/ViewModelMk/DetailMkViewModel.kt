38b3a7e82734aa8d049581eafbb869cb2247e165
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-22 22:05:04 +0700

    data class Variabel yang menyimpan data input form

---
+++ app/src/main/java/com/example/ucp2/ui/ViewModelMk/DetailMkViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.ucp2.ui.ViewModelMk
+
+import com.example.ucp2.data.entity.MataKuliah
+
+
+data class MataKuliahEvent(
+    val code: String ="",
+    val Nama: String="",
+    val SKS: String="",
+    val Semester: String="",
+    val Jenis: String="",
+    val DosenPengampu: String=""
+
+)
+
