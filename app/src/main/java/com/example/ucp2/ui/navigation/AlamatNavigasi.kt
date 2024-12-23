67760c84d31e544b89599f8a6081d2847afa86fa
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-23 21:41:58 +0700

    memperbaiki Alamata Navigasi

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -1,15 +1,33 @@
 package com.example.ucp2.ui.navigation
 
-interface AlamatNavigasi{
+
+
+interface AlamatNavigasi {
     val route: String
 }
 
-object DestinasiHome : AlamatNavigasi{
+object DestinasiHome : AlamatNavigasi {
     override val route = "home"
 }
 
-object DestinasiDetail : AlamatNavigasi{
+object DestinasiDetail : AlamatNavigasi {
     override val route = "detail"
     const val ndin = "ndin"
     val routeWithArg = "$route/{$ndin}"
+}
+
+object DestinasiInsert : AlamatNavigasi {
+    override val route: String = "insert"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val Kode = "Kode"
+    val routeWithArg = "$route/{$Kode}"
+}
+
+object DestinasiDetail1 : AlamatNavigasi {
+    override val route = "detail"
+    const val Kode = "code"
+    val routeWithArg = "$route/{$Kode}"
 }
