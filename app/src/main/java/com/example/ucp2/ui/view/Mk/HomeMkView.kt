e20fb126ca200c706443fb7cb6964c3cc9c589bb
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-23 12:41:07 +0700

    membuat homeMkview dan BodyHomeMkView

--- app/src/main/java/com/example/ucp2/ui/view/Mk/HomeMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/Mk/HomeMkView.kt
@@ -1,29 +1,144 @@
 package com.example.ucp2.ui.view.Mk
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.ui.ViewModelMk.HomeMkViewModel
+import com.example.ucp2.ui.ViewModelMk.HomeUiState
+import kotlinx.coroutines.launch
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.Costumwidget.CustomTopAppBar
+
+@Composable
+fun HomeMkView(
+    viewModel: HomeMkViewModel = viewModel(),
+    onAddMatkul: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                judul = "Daftar Mata Kuliah",
+                showBackButton = false,
+                onBack = { },
+                modifier = Modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMatkul,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mata Kuliah"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMkView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}@Composable
+fun BodyHomeMkView(
+    homeUiState:HomeUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeUiState.isLoading -> {
+            //Menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            LaunchedEffect(Unit) {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = homeUiState.errorMessage ?: "Terjadi kesalahan."
+                    )
+                }
+            }
+        }
 
 
+        homeUiState.listMk.isEmpty() -> {
+            //Menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data mahasiswa.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            //Menampilkan daftar mahasiswa
+            ListMataKuliah(
+                listMk = homeUiState.listMk,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+    }
+}
+}
 @Composable
 fun ListMataKuliah(
     listMk: List<MataKuliah>,
