84e15e08f4457bd9d20ed60005b405d83a89eb2d
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-23 14:35:32 +0700

    menamabah repositroy mk

--- app/src/main/java/com/example/ucp2/dependecieinjection/containerApp.kt
+++ app/src/main/java/com/example/ucp2/dependecieinjection/containerApp.kt
@@ -3,17 +3,24 @@ package com.example.ucp2.dependecieinjection
 import DosenDatabase
 import android.content.Context
 import androidx.room.RoomDatabase
+import com.example.ucp2.data.database.MKDataBase
 import com.example.ucp2.repository.LocalRepositoryDosen
+import com.example.ucp2.repository.LocalRepositoryMK
 import com.example.ucp2.repository.RepositoryDosen
+import com.example.ucp2.repository.RepositoryMK
 
 interface InterfaceContainerApp{
     val RepositoryDosen :RepositoryDosen
+    val repositoryMK : RepositoryMK
 }
 
 
-class ContainerApp(private val context: Context) : InterfaceContainerApp{
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
     override val RepositoryDosen: RepositoryDosen by lazy {
         LocalRepositoryDosen(DosenDatabase.getDatabase(context).dosenDao())
     }
+    override val repositoryMK: RepositoryMK by lazy {
+        LocalRepositoryMK(MKDataBase.getDatabase(context).mkDao())
 
+    }
 }
