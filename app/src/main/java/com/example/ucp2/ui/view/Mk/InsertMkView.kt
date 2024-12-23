4d7cff07d635999dd51be8ac27186041159f53da
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-23 13:11:56 +0700

    membuat Tampilan form mata kuliah dengan tombol simpan.

--- app/src/main/java/com/example/ucp2/ui/view/Mk/InsertMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/Mk/InsertMkView.kt
@@ -1,17 +1,64 @@
 package com.example.ucp2.ui.view.Mk
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.R
 import com.example.ucp2.ui.ViewModelMk.FormErrorState
 import com.example.ucp2.ui.ViewModelMk.MataKuliahEvent
+import com.example.ucp2.ui.ViewModelMk.MataKuliahViewModel
+import com.example.ucp2.ui.ViewModelMk.MkUIState
+
+@Composable
+fun InsertBodyMk(
+    modifier: Modifier = Modifier,
+    viewModel: MataKuliahViewModel = viewModel(),
+    onValueChange: (MataKuliahEvent) -> Unit,
+    uiState: MkUIState,
+    dosenList: List<String>,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMatkul(
+            mataKuliahEvent = uiState.mataKuliahEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            dosenList = dosenList,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.primary),
+                contentColor = Color.White
+            )
+        ) {
+            Text("Simpan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
