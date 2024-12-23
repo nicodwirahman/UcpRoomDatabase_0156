ffa352db9f030b35000263f6de7e6979c987fbed
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-23 21:40:27 +0700

    mengubanah nama dari AppDatabae Ke DatabaseAll

--- app/src/main/java/com/example/ucp2/data/database/DatabaseAll.kt
+++ app/src/main/java/com/example/ucp2/data/database/DatabaseAll.kt
@@ -11,22 +11,22 @@ import com.example.ucp2.data.entity.MataKuliah
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @Database(entities = [Dosen::class, MataKuliah::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {  // Renamed Database to AppDatabase
+abstract class DatabaseAll : RoomDatabase() {  // Renamed Database to AppDatabase
 
     abstract fun dosenDao(): DosenDao
     abstract fun mkDao(): MKDao
 
     companion object {
         @Volatile
-        private var instance: AppDatabase? = null
+        private var instance: DatabaseAll? = null
 
-        @OptIn(InternalCoroutinesApi::class)
-        fun getDatabase(context: Context): AppDatabase {  // Use AppDatabase here
+
+        fun getDatabase(context: Context): DatabaseAll {  // Use AppDatabase here
             return instance ?: synchronized(this) {
                 Room.databaseBuilder(
                     context.applicationContext,
-                    AppDatabase::class.java,  // Use AppDatabase here as well
-                    "AppDatabase"
+                    DatabaseAll::class.java,  // Use AppDatabase here as well
+                    "DatabaseAll"
                 ).build().also { instance = it }
             }
         }
