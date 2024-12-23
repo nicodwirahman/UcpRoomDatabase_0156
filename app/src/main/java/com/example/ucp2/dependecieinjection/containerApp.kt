86fc7c1646d42a098d48de58904c389b3d54374b
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-23 21:40:47 +0700

    memperbaiki ContainerApp

--- app/src/main/java/com/example/ucp2/dependecieinjection/containerApp.kt
+++ app/src/main/java/com/example/ucp2/dependecieinjection/containerApp.kt
@@ -1,26 +1,24 @@
 package com.example.ucp2.dependecieinjection
 
-import DosenDatabase
 import android.content.Context
-import androidx.room.RoomDatabase
-import com.example.ucp2.data.database.MKDataBase
+import com.example.ucp2.data.database.DatabaseAll
 import com.example.ucp2.repository.LocalRepositoryDosen
 import com.example.ucp2.repository.LocalRepositoryMK
 import com.example.ucp2.repository.RepositoryDosen
 import com.example.ucp2.repository.RepositoryMK
 
-interface InterfaceContainerApp{
-    val RepositoryDosen :RepositoryDosen
-    val repositoryMK : RepositoryMK
+
+interface InterfaceContainerApp {
+    val repositoryDosen: RepositoryDosen
+    val repositoryMK: RepositoryMK
 }
 
-
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
-    override val RepositoryDosen: RepositoryDosen by lazy {
-        LocalRepositoryDosen(DosenDatabase.getDatabase(context).dosenDao())
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(DatabaseAll.getDatabase(context).dosenDao()) // Correct database class
     }
-    override val repositoryMK: RepositoryMK by lazy {
-        LocalRepositoryMK(MKDataBase.getDatabase(context).mkDao())
 
+    override val repositoryMK: RepositoryMK by lazy {
+        LocalRepositoryMK(DatabaseAll.getDatabase(context).mkDao()) // Correct database class
     }
 }
